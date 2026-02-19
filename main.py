d1d6b09c4ac76edd0fa62536e8ce8f44e2ed4a2a
kellam <34324060+matt2ology@users.noreply.github.com>
2026-02-19 03:02:00 -0500

    Init commit

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-def main():
-    pass
+import logging
+from system_check import SystemCheck
 
-if _name__ == "__main__":
+
+FORMAT = '[%(asctime)s]-[%(funcName)s]-[%(levelname)s] - %(message)s'
+logging.basicConfig(
+    level=logging.INFO,
+    format=FORMAT
+)
+
+
+def main():
+    logging.info("Starting thermal printer service")
+
+    if not SystemCheck.system_ready():
+        logging.error("System checks failed. Exiting.")
+        return
+
+    logging.info("System ready. Initializing printer...")
+
+
+if __name__ == "__main__":
     main()
